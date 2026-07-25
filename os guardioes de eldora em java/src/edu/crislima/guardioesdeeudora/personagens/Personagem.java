949ea6ccb11fa8c4475137cbf21516e471012b9b
Cristhiano Rodrigues <rodriguescristhiano4@gmail.com>
2026-07-25 12:00:03 -0300

    correção de erros e abertura dos arquivos  no vscode

--- os guardioes de eldora em java/src/edu/crislima/guardioesdeeudora/personagens/Personagem.java
+++ os guardioes de eldora em java/src/edu/crislima/guardioesdeeudora/personagens/Personagem.java
@@ -18,11 +18,11 @@ public class Personagem {
 
     //Metodo para exibir as informações dos pesonagem
     public void exibirInfoPersonagem(){
-        System.out.println("----- Exibindo personagem ----- " );
+        System.out.println("\n----- Exibindo personagem ----- \n" );
         System.out.println("Nome do personagem: " + nome);
         System.out.println("Classe do personagem: " + classe);
         System.out.println("Nivel do Personagem: " + nivelPersonagem);
         System.out.println("Pontos de vida do personagem: " + pontosDeVida);
-        System.out.println("Poder Base do personagwm: " + poderBase);
+        System.out.println("Poder Base do personagem: " + poderBase);
     }
 }
