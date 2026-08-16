a61e9ed00738f26e052b44bdba0b8b04fa0b98aa
Cristhiano Rodrigues <rodriguescristhiano4@gmail.com>
2026-08-16 12:31:34 -0300

    aplicação do que foi feito na Main

--- os guardioes de eldora em java/src/edu/crislima/guardioesdeeudora/Main.java
+++ os guardioes de eldora em java/src/edu/crislima/guardioesdeeudora/Main.java
@@ -1,10 +1,24 @@
 package edu.crislima.guardioesdeeudora;
 
+import edu.crislima.guardioesdeeudora.personagens.Arqueiro;
+import edu.crislima.guardioesdeeudora.personagens.Guerreiro;
+import edu.crislima.guardioesdeeudora.personagens.Mago;
 import edu.crislima.guardioesdeeudora.personagens.Personagem;
 
-public class Main {
-    static void main(String[] args) {
+import java.util.ArrayList;
 
+public class Main {
+    static void main() {
+        //inicialização dos personagens
+        ArrayList<Personagem> personagens = new ArrayList<Personagem>();
+        personagens.add(new Guerreiro("Ragnar", 8,120,12.5));
+        personagens.add(new Arqueiro("Anna", 10,200,22.0));
+        personagens.add(new Mago("Athena",30,80,18.9));
+
+        //exibição dos personagens
+        for (Personagem personagem : personagens) {
+            personagem.usarHabilidadeEspecial();
+        }
        
     }
 }
